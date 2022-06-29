f3e4ce45a1e9727b900d8af78aad272ce2e8c007
Tiger Nie <tnie@tuta.io>
2022-06-29 14:04:56 -0500

    debug option cli

--- imv/__main__.py
+++ imv/__main__.py
@@ -6,6 +6,9 @@ from imv.main import app
 parser = argparse.ArgumentParser()
 parser.add_argument("--host", default="0.0.0.0", help="Server host")
 parser.add_argument("--port", type=int, default=7070, help="Server port")
+parser.add_argument("-d", "--debug", help="Debug mode", action="store_true")
 args = parser.parse_args()
 
-app.run(debug=False, host=args.host, port=args.port)
+print(args)
+
+app.run(debug=args.debug, host=args.host, port=args.port)
